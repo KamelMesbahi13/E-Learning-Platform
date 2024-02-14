ac947660d68ed1f98cf64ab73675e2380a481d45
Kamel Mesbahi <elkamel.mesbahi@gmail.com>
2024-02-14 14:51:02 +0100

    Fix Responsive Commit

--- src/Components/Home/Courses/Courses.jsx
+++ src/Components/Home/Courses/Courses.jsx
@@ -10,7 +10,7 @@ const Teachers = () => {
       <div>
         <div className="container">
           <div>
-            <div className="w-full xl:px-16 mt-20 lg:w-10/12">
+            <div className="w-full xl:px-16 md:mt-20 mt-4 lg:w-10/12">
               <div className="mb-4 font-bold relative before:absolute before:hidden before:xl:block before:h-2 before:w-12 rtl:before:left-[56rem] ltr:before:-left-16 before:bg-third-color before:top-[1.2rem]">
                 <h1 className="mb-2">
                   {t('home_courses_heading')} <br />{' '}
