09f5d9cca1e14a82658960272c4b5f270da013a1
Kamel Mesbahi <elkamel.mesbahi@gmail.com>
2024-02-14 16:47:25 +0100

    Fix Responsive Commit

--- src/Components/Faq/Header/Header.jsx
+++ src/Components/Faq/Header/Header.jsx
@@ -10,7 +10,7 @@ const Header = () => {
     <>
       <div>
         <div className="container">
-          <div className="mt-20 mb-16 md:mb-32 text-center">
+          <div className="mt-20 mb-8 md:mb-32 text-center">
             <h1 className="font-bold">{t('fa_question')}</h1>
             <p className="mt-8 md:w-1/2 mx-auto">{t('faq_paragraph')}</p>
           </div>
